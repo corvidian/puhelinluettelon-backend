83b3c9ef1861e2dab9ad9c033f77d9b5fa6096ec
Topias Heinonen <topias.heinonen@gofore.com>
2022-04-19 21:54:57 +0300

    Tehtävä 3.14

--- index.js
+++ index.js
@@ -97,27 +97,13 @@ app.post("/api/persons", (request, response) => {
     });
   }
 
-  if (persons.some((person) => person.name === body.name)) {
-    return response.status(400).json({
-      error: "name must be unique",
-    });
-  }
-
-  const person = {
+  const person = new Person({
     name: body.name,
     number: body.number,
-    id: generateId(),
-  };
+  });
 
-  persons = persons.concat(person);
-
-  response.json(person);
+  person.save().then((savedPerson) => response.json(savedPerson));
 });
-
-const generateId = () => {
-  const big_random = Math.random() * Number.MAX_SAFE_INTEGER;
-  return Math.floor(big_random);
-};
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
